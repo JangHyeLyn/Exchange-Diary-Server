2b8dd79bb631ee0397b70ef91c33001d944ad878
taxijjang <gw9122@naver.com>
2020-12-27 20:02:05 +0900

    feat:#7-user api 작성

--- apps/Diary/views.py
+++ apps/Diary/views.py
@@ -1,10 +1,10 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from .models import Diary
-from .serializers.user_diary_set_sz import UserDiarySetSZ
+from .serializers.diary_list_sz import DiaryListSZ
 
 
 class DiaryViewSet(ModelViewSet):
     queryset = Diary.objects.all()
-    serializer_class = UserDiarySetSZ
+    serializer_class = DiaryListSZ
     permission_classes = [IsAuthenticated]
