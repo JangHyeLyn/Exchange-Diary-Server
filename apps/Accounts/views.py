ca2f97df8928ccfcb366bc5abd27aa7e98d038c0
taxijjang <gw9122@naver.com>
2021-02-08 16:46:28 +0900

    feat:#16-user리팩토링

--- apps/Accounts/views.py
+++ apps/Accounts/views.py
@@ -1,17 +1,28 @@
-from django.db.models import Q
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from .models import User
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .permissions import IsSelf
 from .serializers import UserSZ
 from django.contrib.auth import get_user_model
+
 
 class UserViewSet(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSZ
-    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        qs = qs.filter(
-            Q(email=self.request.user.email)
-        )
-        return qs
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [IsAdminUser]
+        elif self.action == "create" or self.action == "retrieve":
+            permission_classes = [AllowAny]
+        elif self.action == "group":
+            permission_classes = [IsSelf]
+        else:
+            permission_classes = [IsSelf]
+        return [permission() for permission in permission_classes]
+
+    @action(detail=True)
+    def group(self, request, pk):
+        return Response(data="123123123")
