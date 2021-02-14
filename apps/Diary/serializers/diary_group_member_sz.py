5c12f0576c121db94d8249151bf3cfe50da21dd8
taxijjang <gw9122@naver.com>
2021-02-14 18:18:19 +0900

    feat:#29-diary_title method fields추가

--- apps/Diary/serializers/diary_group_member_sz.py
+++ apps/Diary/serializers/diary_group_member_sz.py
@@ -1,12 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from ..models import DiaryGroupMember
 
 
 class DiaryGroupMemberSZ(ModelSerializer):
+    diary_title = SerializerMethodField()
     class Meta:
         model = DiaryGroupMember
-        fields = ['id', 'rank', 'group', 'diary', 'created_at', 'updated_at']
+        fields = ['id', 'rank', 'group', 'diary', 'diary_title', 'created_at', 'updated_at']
 
     def create(self, validated_data):
         member = DiaryGroupMember.objects.create(**validated_data)
         return member
+
+    def get_diary_title(self, obj):
+        return obj.diary.title
