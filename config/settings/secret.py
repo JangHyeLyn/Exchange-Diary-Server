dc3d79e97ad2c2f10476369c234abdb2afc557d5
taekyun-quriously <taekyun@quriously.com>
2021-04-05 01:39:23 +0900

    s3 IAM change

---
+++ config/settings/secret.py
@@ -0,0 +1,31 @@
+SECRET = {
+    'SECRET_KEY': '4j2mz03tg+dpw_m48=e)i5)1^de%a=7fzc++juwounvs@o^m@n',
+
+    ## Oauth providers
+    'SOCIALACCOUNT_PROVIDERS': {
+        'kakao': {
+            'APP': {
+                'client_id': 'de4e96410888c9be02304c66371ea680',
+                'secret': 520682,
+                'key': ''
+            }
+        }
+    },
+
+    'DATABASE': {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'exchange',
+            'USER': 'root',
+            'PASSWORD': 'schwisestudy',
+            'HOST': 'wisestudy.cinqw7ouyrxc.ap-northeast-2.rds.amazonaws.com',
+            'PORT': 3306,
+            'OPTIONS': {
+                'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+            }
+        }
+    },
+
+    'AWS_ACCESS_KEY_ID': 'AKIAWVQALOFDSSIOX7S2',
+    'AWS_SECRET_ACCESS_KEY': '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'
+}
